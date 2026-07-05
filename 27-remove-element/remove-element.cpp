833036ba7350576ffccfb54bf066011e9e2269b8
Manish-Gowda-T <manishgowdat69@gmail.com>
2026-07-06 02:36:22 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (84.60%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -3,7 +3,6 @@ public:
     int removeElement(vector<int>& nums, int val) {
         int i=0;
         int n=nums.size();
-        sort(nums.begin(),nums.end());
         for(int j=0 ; j < n; j++){
             if(nums[j] != val){
                 nums[i]=nums[j];

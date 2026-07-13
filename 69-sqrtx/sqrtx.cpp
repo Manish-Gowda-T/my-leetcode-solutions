c2452777bd988491884cab9c40a4bfdfed6f04fa
Manish-Gowda-T <manishgowdat69@gmail.com>
2026-07-13 16:12:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (49.80%) - LeetSync

---
+++ 69-sqrtx/sqrtx.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int low=0;
+        int high=x;
+        int ans;
+        while( low <= high){
+            int mid =( low + high)/2;
+            long long square=1LL* mid * mid;
+            if(square == x){
+                return mid;
+            }
+            else if(square < x){
+                ans=mid;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return ans;
+        
+    }
+};

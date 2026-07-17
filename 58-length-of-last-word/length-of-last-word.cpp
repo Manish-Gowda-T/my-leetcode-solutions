e1fe5dea74dc1119dd8645abe2d68da670a66119
Manish-Gowda-T <manishgowdat69@gmail.com>
2026-07-17 11:00:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (68.67%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int end=s.size()-1;
+        while( end>=0 && s[end] ==' '){
+            end--;
+        }
+        int count=0;
+        while ( end>=0 && s[end] != ' '){
+            count++;
+            end--;
+        }
+        return count;
+    }
+};
